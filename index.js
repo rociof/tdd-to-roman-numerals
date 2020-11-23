e48f3485e127721cf1d12ef1d18188ef5844d58c
Aula <Aula@EQUIP079576.xunta.local>
2020-11-23 11:40:07 +0100

    numeros del 1 al 10

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-
-
 /**
  * Conversión a números romanos.
  * @param {number} n - Un número entre 1 y 3999.
  * @returns {string} El equivalente en números romanos.
  */
 export function toRoman(n) {
-  return "I";
-}
+  let resultado = "";
+  if (n == 4) {
+    resultado = "IV";
+    n -= 4;
+  } 
+  if (n == 9) {
+    resultado = "IX";
+    n -= 9;
+  } 
 
+  if (n == 10) {
+    resultado = "X";
+    n -= 10;
+  } 
+  else if (n >= 5) {
+    resultado += "V";
+    n -= 5;
+  }
+
+  for (let i = 0; i < n; i++) {
+    resultado += "I";
+  }
+  
+
+  return resultado;
+}
 
--- test.js
+++ test.js
@@ -2,5 +2,13 @@ import { toRoman } from './index';
 import { strictEqual } from "assert";
 
 describe(('Conversión a números romanos'), () => {
-  it('test 1', () => { strictEqual(toRoman(1), "I") })
+  it('test 1', () => { strictEqual(toRoman(1), "I") });
+  it('test 2', () => { strictEqual(toRoman(2), "II") });
+  it('test 3', () => { strictEqual(toRoman(3), "III") });
+  it('test 4', () => { strictEqual(toRoman(4), "IV") });
+  it('test 5', () => { strictEqual(toRoman(5), "V") });
+  it('test 7', () => { strictEqual(toRoman(7), "VII") });
+  it('test 9', () => { strictEqual(toRoman(9), "IX") });
+  it('test 10', () => { strictEqual(toRoman(10), "X") });
+
 })
