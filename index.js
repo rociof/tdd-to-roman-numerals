1247c7d8032d634042d7ad8c3cf95264775e6a60
Aula <Aula@EQUIP079576.xunta.local>
2020-11-23 13:40:30 +0100

    números hasta 3999

--- index.js
+++ index.js
@@ -4,30 +4,66 @@
  * @returns {string} El equivalente en números romanos.
  */
 export function toRoman(n) {
-  let resultado = "";
-  if (n == 4) {
-    resultado = "IV";
-    n -= 4;
-  } 
-  if (n == 9) {
-    resultado = "IX";
-    n -= 9;
-  } 
+  let resultado = ""
+  while (n >= 1000) {
+    resultado += "M";
+    n -= 1000;
+  }
+  if (n >= 900) {
+    resultado += "CM";
+    n -= 900;
+  }
+  if (n >= 500) {
+    resultado += "D";
+    n -= 500;
+  }
+  if (n >= 400) {
+    resultado += "CD";
+    n -= 400;
+  }
 
-  if (n == 10) {
-    resultado = "X";
+  while (n >= 100) {
+    resultado += "C";
+    n -= 100;
+  }
+  if (n >= 90) {
+    resultado += "XC";
+    n -= 90;
+  }
+
+
+  if (n >= 50) {
+    resultado += "L";
+    n -= 50;
+  }
+
+  if (n >= 40) {
+    resultado += "XL";
+    n -= 40;
+  }
+
+  while (n >= 10) {
+    resultado += "X";
     n -= 10;
-  } 
-  else if (n >= 5) {
+  }
+
+  if (n == 9) {
+    resultado += "IX";
+    n -= 9;
+  }
+
+  if (n == 4) {
+    resultado += "IV";
+    n -= 4;
+  }
+
+  if (n >= 5) {
     resultado += "V";
     n -= 5;
   }
-
   for (let i = 0; i < n; i++) {
     resultado += "I";
   }
-  
 
   return resultado;
 }
-
--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 import { toRoman } from './index';
 import { strictEqual } from "assert";
+import { clearScreenDown } from 'readline';
 
 describe(('Conversión a números romanos'), () => {
   it('test 1', () => { strictEqual(toRoman(1), "I") });
@@ -9,6 +10,30 @@ describe(('Conversión a números romanos'), () => {
   it('test 5', () => { strictEqual(toRoman(5), "V") });
   it('test 7', () => { strictEqual(toRoman(7), "VII") });
   it('test 9', () => { strictEqual(toRoman(9), "IX") });
-  it('test 10', () => { strictEqual(toRoman(10), "X") });
+  it('test 11', () => { strictEqual(toRoman(11), "XI") });
+  it('test 14', () => { strictEqual(toRoman(14), "XIV") });
+  it('test 16', () => { strictEqual(toRoman(16), "XVI") });
+  it('test 20', () => { strictEqual(toRoman(20), "XX") });
+  it('test 35', () => { strictEqual(toRoman(35), "XXXV") });
+  it('test 40', () => { strictEqual(toRoman(40), "XL") });
+  it('test 49', () => { strictEqual(toRoman(49), "XLIX") });
+  it('test 50', () => { strictEqual(toRoman(50), "L") });
+  it('test 69', () => { strictEqual(toRoman(69), "LXIX") });
+  it('test 90', () => { strictEqual(toRoman(90), "XC") });
+  it('test 100', () => { strictEqual(toRoman(100), "C") });
+  it('test 111', () => { strictEqual(toRoman(111), "CXI") });
+  it('test 400', () => { strictEqual(toRoman(400), "CD") });
+  it('test 500', () => { strictEqual(toRoman(500), "D") });
+  it('test 666', () => { strictEqual(toRoman(666), "DCLXVI") });
+  it('test 900', () => { strictEqual(toRoman(900), "CM") });
+  it('test 999', () => { strictEqual(toRoman(999), "CMXCIX") });
+  it('test 1000', () => { strictEqual(toRoman(1000), "M") });
+  it('test 1500', () => { strictEqual(toRoman(1500), "MD") });
+  it('test 3000', () => { strictEqual(toRoman(3000), "MMM") });
+  it('test 3999', () => { strictEqual(toRoman(3999), "MMMCMXCIX") });
 
+  
+
+  
 })
+clearScreenDown
