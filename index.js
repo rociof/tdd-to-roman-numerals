e9757a0680a1eb54a2f5fd573ed7aeb5827fefc7
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-11-23 09:13:02 +0100

    Actualización de dependencias y elementos obsoletos.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 
 
 /**
- * Conversión a números romanos
- * @param {*} n - Un número
+ * Conversión a números romanos.
+ * @param {number} n - Un número entre 1 y 3999.
+ * @returns {string} El equivalente en números romanos.
  */
 export function toRoman(n) {
   return "I";
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 import { toRoman } from './index';
-import { equal } from "assert";
+import { strictEqual } from "assert";
 
 describe(('Conversión a números romanos'), () => {
-  it('test 1', () => { equal(toRoman(1), "I") })
+  it('test 1', () => { strictEqual(toRoman(1), "I") })
 })
